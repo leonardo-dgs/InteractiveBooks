79677c50485f465d9f23aef969277b1f790acda4
Leomixer17 <leonardo.dgs@yahoo.com>
2018-10-06 19:41:08 +0200

    Initial commit

---
+++ core/src/main/java/net/leomixer17/interactivebooks/InteractiveBooks.java
@@ -0,0 +1,67 @@
+package net.leomixer17.interactivebooks;
+
+import net.leomixer17.interactivebooks.nms.IBooksUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class InteractiveBooks extends JavaPlugin {
+	
+	private static InteractiveBooks plugin;
+	private static final Map<String, IBook> books = new HashMap<>();
+	private static IBooksUtils iBooksUtils;
+	
+	@Override
+	public void onEnable()
+	{
+		plugin = this;
+		Config.loadAll();
+		iBooksUtils = IBooksUtils.setupIBooksUtils();
+		if(iBooksUtils == null) {
+			this.getLogger().severe("This Bukkit version is not supported. Disabling...");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
+		this.getCommand("ibooks").setExecutor(new CommandIBooks());
+		Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		plugin = null;
+	}
+	
+	public static InteractiveBooks getPlugin()
+	{
+		return plugin;
+	}
+	
+	public static Map<String, IBook> getBooks()
+	{
+		return new HashMap<>(books);
+	}
+	
+	public static IBook getBook(final String id)
+	{
+		return books.get(id);
+	}
+	
+	public static void registerBook(final IBook book)
+	{
+		books.put(book.getId(), book);
+	}
+	
+	public static void unregisterBook(final String id)
+	{
+		books.remove(id);
+	}
+	
+	static IBooksUtils getIBooksUtils()
+	{
+		return iBooksUtils;
+	}
+	
+}
---
+++ core/src/main/java/net/leomixer17/interactivebooks/nms/TextComponentBuilder.java
@@ -0,0 +1,65 @@
+package net.leomixer17.interactivebooks.nms;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+final class TextComponentBuilder {
+	
+	private final List<TextComponent> components = new ArrayList<>();
+	private ClickEvent nextClickEvent;
+	private HoverEvent nextHoverEvent;
+	
+	TextComponentBuilder()
+	{
+		
+	}
+	
+	List<TextComponent> getComponents()
+	{
+		return this.components;
+	}
+	
+	void add(final TextComponent component)
+	{
+		if(this.getNextClickEvent() != null)
+			component.setClickEvent(this.getNextClickEvent());
+		if(this.getNextHoverEvent() != null)
+			component.setHoverEvent(this.getNextHoverEvent());
+		this.components.add(component);
+	}
+	
+	void add(final TextComponentBuilder builder)
+	{
+		builder.getComponents().forEach(component -> this.add(component));
+	}
+	
+	void add(final List<TextComponent> components)
+	{
+		components.forEach(component -> this.add(component));
+	}
+	
+	public ClickEvent getNextClickEvent()
+	{
+		return this.nextClickEvent;
+	}
+	
+	public void setNextClickEvent(final ClickEvent nextClickEvent)
+	{
+		this.nextClickEvent = nextClickEvent;
+	}
+	
+	public HoverEvent getNextHoverEvent()
+	{
+		return this.nextHoverEvent;
+	}
+	
+	public void setNextHoverEvent(final HoverEvent nextHoverEvent)
+	{
+		this.nextHoverEvent = nextHoverEvent;
+	}
+	
+}
