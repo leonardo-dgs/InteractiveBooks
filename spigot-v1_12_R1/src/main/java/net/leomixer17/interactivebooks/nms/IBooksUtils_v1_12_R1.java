c710f06aea37eccbbf3e82d2bbe722a0c02d031b
leonardo-dgs <17832817+leonardo-dgs@users.noreply.github.com>
2018-10-10 23:55:10 +0200

    Code reformatted

--- spigot-v1_12_R1/src/main/java/net/leomixer17/interactivebooks/nms/IBooksUtils_v1_12_R1.java
+++ spigot-v1_12_R1/src/main/java/net/leomixer17/interactivebooks/nms/IBooksUtils_v1_12_R1.java
@@ -19,51 +19,51 @@ import java.util.List;
 
 final class IBooksUtils_v1_12_R1 implements IBooksUtils {
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public BookMeta getBookMeta(final BookMeta meta, final List<String> rawPages, final Player player)
-	{
-		final ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
-		final BookMeta bookMeta = (BookMeta) book.getItemMeta();
-		bookMeta.setDisplayName(meta.getDisplayName());
-		bookMeta.setTitle(meta.getTitle());
-		bookMeta.setAuthor(meta.getAuthor());
-		bookMeta.setLore(meta.getLore());
-		if (IBooksUtils.hasPlaceholderAPISupport())
-			IBooksUtils.replacePlaceholders(bookMeta, player);
-		else
-			IBooksUtils.replaceColorCodes(bookMeta);
-		List<IChatBaseComponent> pages = null;
-		try
-		{
-			pages = (List<IChatBaseComponent>) CraftMetaBook.class.getDeclaredField("pages").get(bookMeta);
-		}
-		catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e)
-		{
-			e.printStackTrace();
-		}
-		pages.addAll(this.getPages(bookMeta, rawPages, player));
+    @SuppressWarnings("unchecked")
+    @Override
+    public BookMeta getBookMeta(final BookMeta meta, final List<String> rawPages, final Player player)
+    {
+        final ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+        final BookMeta bookMeta = (BookMeta) book.getItemMeta();
+        bookMeta.setDisplayName(meta.getDisplayName());
+        bookMeta.setTitle(meta.getTitle());
+        bookMeta.setAuthor(meta.getAuthor());
+        bookMeta.setLore(meta.getLore());
+        if (IBooksUtils.hasPlaceholderAPISupport())
+            IBooksUtils.replacePlaceholders(bookMeta, player);
+        else
+            IBooksUtils.replaceColorCodes(bookMeta);
+        List<IChatBaseComponent> pages = null;
+        try
+        {
+            pages = (List<IChatBaseComponent>) CraftMetaBook.class.getDeclaredField("pages").get(bookMeta);
+        }
+        catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e)
+        {
+            e.printStackTrace();
+        }
+        pages.addAll(this.getPages(bookMeta, rawPages, player));
 
-		return bookMeta;
-	}
+        return bookMeta;
+    }
 
-	@Override
-	public List<IChatBaseComponent> getPages(final BookMeta meta, final List<String> rawPages, final Player player)
-	{
-		final List<IChatBaseComponent> pages = new ArrayList<IChatBaseComponent>();
-		rawPages.forEach(page -> pages.add(ChatSerializer.a(ComponentSerializer.toString(IBooksUtils.getPage(page, player)))));
-		return pages;
-	}
+    @Override
+    public List<IChatBaseComponent> getPages(final BookMeta meta, final List<String> rawPages, final Player player)
+    {
+        final List<IChatBaseComponent> pages = new ArrayList<IChatBaseComponent>();
+        rawPages.forEach(page -> pages.add(ChatSerializer.a(ComponentSerializer.toString(IBooksUtils.getPage(page, player)))));
+        return pages;
+    }
 
-	@Override
-	public void openBook(final ItemStack book, final Player player)
-	{
-		final int slot = player.getInventory().getHeldItemSlot();
-		final ItemStack old = player.getInventory().getItem(slot);
-		player.getInventory().setItem(slot, book);
-		final PacketPlayOutCustomPayload packet = new PacketPlayOutCustomPayload("MC|BOpen", new PacketDataSerializer(Unpooled.buffer()).a(EnumHand.MAIN_HAND));
-		((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
-		player.getInventory().setItem(slot, old);
-	}
+    @Override
+    public void openBook(final ItemStack book, final Player player)
+    {
+        final int slot = player.getInventory().getHeldItemSlot();
+        final ItemStack old = player.getInventory().getItem(slot);
+        player.getInventory().setItem(slot, book);
+        final PacketPlayOutCustomPayload packet = new PacketPlayOutCustomPayload("MC|BOpen", new PacketDataSerializer(Unpooled.buffer()).a(EnumHand.MAIN_HAND));
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+        player.getInventory().setItem(slot, old);
+    }
 
 }
