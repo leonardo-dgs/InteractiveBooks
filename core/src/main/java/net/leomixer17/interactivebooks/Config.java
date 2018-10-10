201d6fa33baa86b8f41f97a46410bf46a1202e05
Leomixer17 <leonardo.dgs@yahoo.com>
2018-10-10 23:55:10 +0200

    Code reformatted

--- core/src/main/java/net/leomixer17/interactivebooks/Config.java
+++ core/src/main/java/net/leomixer17/interactivebooks/Config.java
@@ -7,32 +7,32 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 final class Config {
-	
-	static final void loadAll()
-	{
-		InteractiveBooks.getPlugin().saveDefaultConfig();
-		final File f = new File(InteractiveBooks.getPlugin().getDataFolder(), "books");
-		if (!f.exists())
-		{
-			f.mkdirs();
-			try
-			{
-				Files.copy(InteractiveBooks.getPlugin().getResource("examplebook.yml"), new File(f, "examplebook.yml").toPath());
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-		loadBookConfigs();
-	}
-	
-	static final void loadBookConfigs()
-	{
-		InteractiveBooks.getBooks().keySet().forEach(id -> InteractiveBooks.unregisterBook(id));
-		final File booksFolder = new File(InteractiveBooks.getPlugin().getDataFolder(), "books");
-		for (final File f : booksFolder.listFiles())
-			if (f.getName().endsWith(".yml"))
-				InteractiveBooks.registerBook(new IBook(f.getName().substring(0, f.getName().length() - 4), YamlConfiguration.loadConfiguration(f)));
-	}
+    
+    static final void loadAll()
+    {
+        InteractiveBooks.getPlugin().saveDefaultConfig();
+        final File f = new File(InteractiveBooks.getPlugin().getDataFolder(), "books");
+        if (!f.exists())
+        {
+            f.mkdirs();
+            try
+            {
+                Files.copy(InteractiveBooks.getPlugin().getResource("examplebook.yml"), new File(f, "examplebook.yml").toPath());
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        loadBookConfigs();
+    }
+    
+    static final void loadBookConfigs()
+    {
+        InteractiveBooks.getBooks().keySet().forEach(id -> InteractiveBooks.unregisterBook(id));
+        final File booksFolder = new File(InteractiveBooks.getPlugin().getDataFolder(), "books");
+        for (final File f : booksFolder.listFiles())
+            if (f.getName().endsWith(".yml"))
+                InteractiveBooks.registerBook(new IBook(f.getName().substring(0, f.getName().length() - 4), YamlConfiguration.loadConfiguration(f)));
+    }
 }
--- core/src/main/java/net/leomixer17/interactivebooks/PlayerListener.java
+++ core/src/main/java/net/leomixer17/interactivebooks/PlayerListener.java
@@ -13,67 +13,67 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
 public final class PlayerListener implements Listener {
-	
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerJoin(final PlayerJoinEvent event)
-	{
-		if (event.getPlayer().hasPlayedBefore())
-		{
-			InteractiveBooks.getPlugin().getConfig().getStringList("books_on_join").forEach(id -> {
-				if (InteractiveBooks.getBook(id) != null)
-					event.getPlayer().getInventory().addItem(InteractiveBooks.getBook(id).getItem(event.getPlayer()));
-			});
-		}
-		else
-		{
-			InteractiveBooks.getPlugin().getConfig().getStringList("books_on_first_join").forEach(id -> {
-				if (InteractiveBooks.getBook(id) != null)
-					event.getPlayer().getInventory().addItem(InteractiveBooks.getBook(id).getItem(event.getPlayer()));
-			});
-		}
-	}
-	
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerInteract(final PlayerInteractEvent e)
-	{
-		if (!e.getAction().equals(Action.RIGHT_CLICK_AIR) && !e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
-			return;
-		if (!InteractiveBooks.getPlugin().getConfig().getBoolean("update_books_on_use"))
-			return;
-		if (!IBooksUtils.getItemInMainHand(e.getPlayer()).getType().equals(Material.WRITTEN_BOOK))
-			return;
-		final NBTItem nbti = new NBTItem(IBooksUtils.getItemInMainHand(e.getPlayer()));
-		if (!nbti.hasKey("InteractiveBooks|Book-Id"))
-			return;
-		final IBook book = InteractiveBooks.getBook(nbti.getString("InteractiveBooks|Book-Id"));
-		if (book == null)
-			return;
-		final ItemStack bookItem = book.getItem(e.getPlayer());
-		bookItem.setAmount(IBooksUtils.getItemInMainHand(e.getPlayer()).getAmount());
-		IBooksUtils.setItemInMainHand(e.getPlayer(), bookItem);
-	}
-	
-	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-	public void onPlayerCommandPreprocess(final PlayerCommandPreprocessEvent event)
-	{
-		final String command = event.getMessage().split(" ", 2)[0].replaceFirst("/", "").toLowerCase();
-		IBook iBook = null;
-		for (final IBook book : InteractiveBooks.getBooks().values())
-			if (book.getOpenCommands().contains(command))
-			{
-				iBook = book;
-				break;
-			}
-		if (iBook == null)
-			return;
-		if (!event.getPlayer().hasPermission("interactivebooks.open." + iBook.getId()))
-		{
-			event.getPlayer().sendMessage("§cYou don't have permission to open this book.");
-			return;
-		}
-		
-		iBook.open(event.getPlayer());
-		event.setCancelled(true);
-	}
-	
+    
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(final PlayerJoinEvent event)
+    {
+        if (event.getPlayer().hasPlayedBefore())
+        {
+            InteractiveBooks.getPlugin().getConfig().getStringList("books_on_join").forEach(id -> {
+                if (InteractiveBooks.getBook(id) != null)
+                    event.getPlayer().getInventory().addItem(InteractiveBooks.getBook(id).getItem(event.getPlayer()));
+            });
+        }
+        else
+        {
+            InteractiveBooks.getPlugin().getConfig().getStringList("books_on_first_join").forEach(id -> {
+                if (InteractiveBooks.getBook(id) != null)
+                    event.getPlayer().getInventory().addItem(InteractiveBooks.getBook(id).getItem(event.getPlayer()));
+            });
+        }
+    }
+    
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerInteract(final PlayerInteractEvent e)
+    {
+        if (!e.getAction().equals(Action.RIGHT_CLICK_AIR) && !e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
+            return;
+        if (!InteractiveBooks.getPlugin().getConfig().getBoolean("update_books_on_use"))
+            return;
+        if (!IBooksUtils.getItemInMainHand(e.getPlayer()).getType().equals(Material.WRITTEN_BOOK))
+            return;
+        final NBTItem nbti = new NBTItem(IBooksUtils.getItemInMainHand(e.getPlayer()));
+        if (!nbti.hasKey("InteractiveBooks|Book-Id"))
+            return;
+        final IBook book = InteractiveBooks.getBook(nbti.getString("InteractiveBooks|Book-Id"));
+        if (book == null)
+            return;
+        final ItemStack bookItem = book.getItem(e.getPlayer());
+        bookItem.setAmount(IBooksUtils.getItemInMainHand(e.getPlayer()).getAmount());
+        IBooksUtils.setItemInMainHand(e.getPlayer(), bookItem);
+    }
+    
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerCommandPreprocess(final PlayerCommandPreprocessEvent event)
+    {
+        final String command = event.getMessage().split(" ", 2)[0].replaceFirst("/", "").toLowerCase();
+        IBook iBook = null;
+        for (final IBook book : InteractiveBooks.getBooks().values())
+            if (book.getOpenCommands().contains(command))
+            {
+                iBook = book;
+                break;
+            }
+        if (iBook == null)
+            return;
+        if (!event.getPlayer().hasPermission("interactivebooks.open." + iBook.getId()))
+        {
+            event.getPlayer().sendMessage("§cYou don't have permission to open this book.");
+            return;
+        }
+        
+        iBook.open(event.getPlayer());
+        event.setCancelled(true);
+    }
+    
 }
