be93302c862eeabbe59ffd969be623abfeeba4c3
leonardo-dgs <17832817+leonardo-dgs@users.noreply.github.com>
2019-09-09 01:08:30 +0200

    Don't tab complete the command for players that doesn't have access to it

--- src/main/java/net/leomixer17/interactivebooks/InteractiveBooks.java
+++ src/main/java/net/leomixer17/interactivebooks/InteractiveBooks.java
@@ -2,10 +2,12 @@ package net.leomixer17.interactivebooks;
 
 import org.bstats.bukkit.MetricsLite;
 import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public final class InteractiveBooks extends JavaPlugin {
 
@@ -17,7 +19,7 @@ public final class InteractiveBooks extends JavaPlugin {
     {
         plugin = this;
         Config.loadAll();
-        this.getCommand("ibooks").setExecutor(new CommandIBooks());
+        registerCommand();
         Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
         new MetricsLite(this);
     }
@@ -51,6 +53,14 @@ public final class InteractiveBooks extends JavaPlugin {
     public static void unregisterBook(final String id)
     {
         books.remove(id);
+    }
+
+    private void registerCommand()
+    {
+        PluginCommand commandIBooks = getCommand("ibooks");
+        Objects.requireNonNull(commandIBooks).setPermission("interactivebooks.command");
+        commandIBooks.setPermissionMessage("§4You don't have permission to access to this command.");
+        commandIBooks.setExecutor(new CommandIBooks());
     }
 
 }
