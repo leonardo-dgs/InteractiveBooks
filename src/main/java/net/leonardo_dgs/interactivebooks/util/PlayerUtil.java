8b898e6ed5736f26ed704e7d58991f2951b112a4
leonardo-dgs <17832817+leonardo-dgs@users.noreply.github.com>
2020-06-21 00:44:56 +0200

    Clean up code

--- src/main/java/net/leonardo_dgs/interactivebooks/util/PlayerUtil.java
+++ src/main/java/net/leonardo_dgs/interactivebooks/util/PlayerUtil.java
@@ -10,34 +10,13 @@ import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Objects;
 
 public final class PlayerUtil {
 
     private static final Object OPENBOOK_PACKET;
 
-    private static final Method ICHATBASECOMPONENT_A_METHOD;
-
-    private static final Object ACTIONBAR_ENUM;
-     private static final Constructor<?> ACTIONBAR_CONSTRUCTOR;
-
     static {
-        Method iChatBaseComponent_A_Method = null;
-        Object actionbar_Enum = null;
-        Constructor<?> actionbar_Constructor = null;
-        try {
-            iChatBaseComponent_A_Method = ServerReflection.nmsClass("IChatBaseComponent").getDeclaredClasses()[0].getMethod("a", String.class);
-            actionbar_Enum = ServerReflection.nmsClass("PacketPlayOutTitle").getDeclaredClasses()[0].getField("ACTIONBAR").get(null);
-            actionbar_Constructor = ServerReflection.nmsClass("PacketPlayOutTitle").getConstructor(ServerReflection.nmsClass("PacketPlayOutTitle").getDeclaredClasses()[0], ServerReflection.nmsClass("IChatBaseComponent"));
-        } catch (IllegalAccessException | NoSuchFieldException | NoSuchMethodException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        ICHATBASECOMPONENT_A_METHOD = iChatBaseComponent_A_Method;
-        ACTIONBAR_ENUM = actionbar_Enum;
-        ACTIONBAR_CONSTRUCTOR = actionbar_Constructor;
-
-        Object openBook_Packet = null;
+        Object openBookPacket = null;
         try {
             Constructor<?> packetConstructor;
             Enum<?> enumHand;
@@ -48,7 +27,7 @@ public final class PlayerUtil {
                 case v1_14_R1:
                     enumHand = (Enum<?>) ServerReflection.nmsClass("EnumHand").getField("MAIN_HAND").get(null);
                     packetConstructor = ServerReflection.nmsClass("PacketPlayOutOpenBook").getConstructor(ServerReflection.nmsClass("EnumHand"));
-                    openBook_Packet = packetConstructor.newInstance(enumHand);
+                    openBookPacket = packetConstructor.newInstance(enumHand);
                     break;
 
                 case v1_13_R2:
@@ -58,7 +37,7 @@ public final class PlayerUtil {
                     packetDataSerializerArg = ServerReflection.nmsClass("PacketDataSerializer").getConstructor(ByteBuf.class).newInstance(Unpooled.buffer());
                     packetDataSerializer = ServerReflection.nmsClass("PacketDataSerializer").getMethod("a", Enum.class).invoke(packetDataSerializerArg, enumHand);
                     packetConstructor = ServerReflection.nmsClass("PacketPlayOutCustomPayload").getConstructor(ServerReflection.nmsClass("MinecraftKey"), ServerReflection.nmsClass("PacketDataSerializer"));
-                    openBook_Packet = packetConstructor.newInstance(minecraftKey, packetDataSerializer);
+                    openBookPacket = packetConstructor.newInstance(minecraftKey, packetDataSerializer);
                     break;
 
                 case v1_12_R1:
@@ -70,7 +49,7 @@ public final class PlayerUtil {
                     packetDataSerializerArg = ServerReflection.nmsClass("PacketDataSerializer").getConstructor(ByteBuf.class).newInstance(Unpooled.buffer());
                     packetDataSerializer = ServerReflection.nmsClass("PacketDataSerializer").getMethod("a", Enum.class).invoke(packetDataSerializerArg, enumHand);
                     packetConstructor = ServerReflection.nmsClass("PacketPlayOutCustomPayload").getConstructor(String.class, ServerReflection.nmsClass("PacketDataSerializer"));
-                    openBook_Packet = packetConstructor.newInstance("MC|BOpen", packetDataSerializer);
+                    openBookPacket = packetConstructor.newInstance("MC|BOpen", packetDataSerializer);
                     break;
 
                 case v1_8_R3:
@@ -78,43 +57,13 @@ public final class PlayerUtil {
                 case v1_8_R1:
                     packetDataSerializer = ServerReflection.nmsClass("PacketDataSerializer").getConstructor(ByteBuf.class).newInstance(Unpooled.buffer());
                     packetConstructor = ServerReflection.nmsClass("PacketPlayOutCustomPayload").getConstructor(String.class, ServerReflection.nmsClass("PacketDataSerializer"));
-                    openBook_Packet = packetConstructor.newInstance("MC|BOpen", packetDataSerializer);
+                    openBookPacket = packetConstructor.newInstance("MC|BOpen", packetDataSerializer);
                     break;
             }
         } catch (NoSuchFieldException | InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-        OPENBOOK_PACKET = openBook_Packet;
-    }
-
-    /**
-     * Sends an action bar to a set of players.
-     *
-     * @param text the action bar text
-     * @param players the players to whom send the action bar
-     */
-    public static void sendActionBar(String text, Player... players) {
-        Objects.requireNonNull(ICHATBASECOMPONENT_A_METHOD);
-        Objects.requireNonNull(ACTIONBAR_CONSTRUCTOR);
-        text = text.replace("\\", "\\\\").replace("\"", "\\\"");
-        try {
-            for (Player player : players) {
-                Object chatText = ICHATBASECOMPONENT_A_METHOD.invoke(null, "{\"text\":\"" + text + "\"}");
-                Object titlePacket = ACTIONBAR_CONSTRUCTOR.newInstance(ACTIONBAR_ENUM, chatText);
-                ReflectionUtil.sendPacket(titlePacket, player);
-            }
-        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Clears the action bar to a set of players.
-     *
-     * @param players the players to whom clear the action bar
-     */
-    public static void clearActionBar(Player... players) {
-        sendActionBar("", players);
+        OPENBOOK_PACKET = openBookPacket;
     }
 
     /**
