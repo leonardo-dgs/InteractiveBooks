5c769fd884cb593653723c614baef41fa3debdc6
leonardo-dgs <17832817+leonardo-dgs@users.noreply.github.com>
2021-01-13 01:34:28 +0100

    Prepare for InteractiveBooks v2

---
+++ Bukkit/src/main/java/net/leonardo_dgs/interactivebooks/bukkit/BukkitMain.java
@@ -0,0 +1,17 @@
+package net.leonardo_dgs.interactivebooks.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
